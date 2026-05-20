481c6e9c9acbe3cffde4834de8df69f5e6a870ec
Tyler <209157553+thall34@users.noreply.github.com>
2026-05-20 13:38:16 -0400

    Add Express framework and refactor index.js

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-Creating a basic information site using Node.js
+Creating a basic information site using Node.js May 20, 2026
+Enhanced using Express framework May 20, 2026
--- index.js
+++ index.js
@@ -1,45 +1,84 @@
-const http = require('http');
-const fs = require('fs');
+// used for old code
+// const http = require('http');
+// const fs = require('fs');
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 8080;
 
-const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/html');
+app.get('/', (req, res) => {
+    res.statusCode = 200;
+    res.sendFile(path.join(__dirname, '/project/index.html'));
+});
 
-    let path = './project/';
-    switch(req.url) {
-        case '/':
-            path += 'index.html';
-            res.statusCode = 200;
-            break;
-        case '/about':
-            path += 'about.html';
-            res.statusCode = 200;
-            break;
-        case '/contact-me':
-            path += 'contact-me.html';
-            res.statusCode = 200;
-            break;
-        case '/contact':
-            res.statusCode = 301;
-            res.setHeader('Location', '/contact-me');
-            res.end();
-            break;
-        default:
-            path += '404.html';
-            res.statusCode = 404;
-            break;
+app.get('/about', (req, res) => {
+    res.statusCode = 200;
+    res.sendFile(path.join(__dirname, '/project/about.html'));
+});
+
+app.get('/contact-me', (req, res) => {
+    res.statusCode = 200;
+    res.sendFile(path.join(__dirname, '/project/contact-me.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.statusCode = 301;
+    res.redirect(path.join(__dirname, '/project/contact-me.html'));
+});
+
+app.use((req, res, next) => {
+    res.statusCode = 404;
+    res.status(404).sendFile(path.join(__dirname, 'project/404.html'));
+})
+
+app.listen(PORT, (err) => {
+    if (err) {
+        console.log(err);
     };
 
-    fs.readFile(path, (err, data) => {
-        if (err) {
-            console.log(err);
-            res.end();
-        } else {
-            res.write(data);
-            res.end();
-        };
-    });
+    console.log(`listening for requests on port ${PORT}`)
 });
 
-server.listen(8080, 'localhost', () => {
-    console.log('listening for requests on port 8080')
-});
+// old code
+// const server = http.createServer((req, res) => {
+//     res.setHeader('Content-Type', 'text/html');
+
+//     let path = './project/';
+//     switch(req.url) {
+//         case '/':
+//             path += 'index.html';
+//             res.statusCode = 200;
+//             break;
+//         case '/about':
+//             path += 'about.html';
+//             res.statusCode = 200;
+//             break;
+//         case '/contact-me':
+//             path += 'contact-me.html';
+//             res.statusCode = 200;
+//             break;
+//         case '/contact':
+//             res.statusCode = 301;
+//             res.setHeader('Location', '/contact-me');
+//             res.end();
+//             break;
+//         default:
+//             path += '404.html';
+//             res.statusCode = 404;
+//             break;
+//     };
+
+//     fs.readFile(path, (err, data) => {
+//         if (err) {
+//             console.log(err);
+//             res.end();
+//         } else {
+//             res.write(data);
+//             res.end();
+//         };
+//     });
+// });
+
+// server.listen(8080, 'localhost', () => {
+//     console.log('listening for requests on port 8080')
+// });
